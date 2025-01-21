70415c246a0c8832037e93a3ab092fa3316c5958
ShukelovichDmitry <mitya.shukelovich@mail.ru>
2025-01-21 20:50:23 +0300

    Откат изменений

--- src/Lesson3/ReverseStairs.java
+++
@@ -1,9 +0,0 @@
-package Lesson3;
-
-public class ReverseStairs {
-    public static void reverseStairs(int N){
-        while(N > 0){
-            System.out.println("#".repeat(N--));
-        }
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,14 @@ import Lesson2.Fibonachi;
 import Lesson2.MultiplyTable;
 import Lesson2.Rectangle;
 import Lesson2.StudentScore;
-import Lesson3.Palindrome;
-import Lesson3.PrimeNumber;
-import Lesson3.ReverseStairs;
-import Lesson3.SumOfDigits;
-import Lesson4.ConvertASCII;
-import Lesson4.ReverseWordsInString;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        ReverseStairs.reverseStairs(10);
+        Rectangle.calculateRectangleFromInput();
+        StudentScore.showGradeFromInputScore();
+        MultiplyTable.printMultiplyTableFromInput();
+        Fibonachi.getFibonachiFromInput();
     }
 
 }

17876b1194d9fdab6a2328d0165b0e0375bd2875
ShukelovichDmitry <mitya.shukelovich@mail.ru>
2025-01-21 21:10:26 +0300

    Откат изменений

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,27 @@ import Lesson2.Fibonachi;
 import Lesson2.MultiplyTable;
 import Lesson2.Rectangle;
 import Lesson2.StudentScore;
+<<<<<<< HEAD
 import Lesson3.Palindrome;
 import Lesson3.PrimeNumber;
 import Lesson4.ConvertASCII;
 import Lesson4.ReverseWordsInString;
 
 import java.util.Scanner;
+=======
+>>>>>>> parent of 6962287 (ДЗ по Уроку 3 и Уроку 4)
 
 public class Main {
 
     public static void main(String[] args){
+<<<<<<< HEAD
         System.out.println(PrimeNumber.isPrimeNumber(117));
+=======
+        Rectangle.calculateRectangleFromInput();
+        StudentScore.showGradeFromInputScore();
+        MultiplyTable.printMultiplyTableFromInput();
+        Fibonachi.getFibonachiFromInput();
+>>>>>>> parent of 6962287 (ДЗ по Уроку 3 и Уроку 4)
     }
 
 }

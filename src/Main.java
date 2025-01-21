607092e961d1ff8c63084693f14abb711236ef1e
ShukelovichDmitry <mitya.shukelovich@mail.ru>
2025-01-21 20:56:12 +0300

    ДЗ по Уроку 3 и Уроку 4

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ import Lesson2.Fibonachi;
 import Lesson2.MultiplyTable;
 import Lesson2.Rectangle;
 import Lesson2.StudentScore;
+import Lesson3.Palindrome;
+import Lesson3.PrimeNumber;
+import Lesson4.ConvertASCII;
+import Lesson4.ReverseWordsInString;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        Rectangle.calculateRectangleFromInput();
-        StudentScore.showGradeFromInputScore();
-        MultiplyTable.printMultiplyTableFromInput();
-        Fibonachi.getFibonachiFromInput();
+        System.out.println(PrimeNumber.isPrimeNumber(117));
     }
 
 }

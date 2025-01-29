4bf19b44383bd4dfe5aa8c887f88e60bfb721acd
ShukelovichDmitry <mitya.shukelovich@mail.ru>
2025-01-29 13:52:14 +0300

    ДЗ Урок 6

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ import Lesson5.ArrayPalindrome;
 import Lesson5.FindMaxMin;
 import Lesson5.OddAndEven;
 import Lesson5.ReverseArray;
+import Lesson6.CountReplacement;
+import Lesson6.FrequencySort;
+import Lesson6.SortWithNegativeNumbers;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -18,10 +21,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println(PrimeNumber.isPrimeNumber(117));
-        Rectangle.calculateRectangleFromInput();
-        StudentScore.showGradeFromInputScore();
-        MultiplyTable.printMultiplyTableFromInput();
-        Fibonachi.getFibonachiFromInput();
+        int[] arr = new int[]{4, -5, -6, 6, 5, -3, -6, -1, 2};
+        /*Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+        }*/
+        //FrequencySort.sort(arr);
+        //SortWithNegativeNumbers.sort(arr);
+        int count = CountReplacement.sort(arr, 0, arr.length-1);
+        System.out.println("Результат: " + count);
+        System.out.println("Результат: " + Arrays.toString(arr));
     }
 }
